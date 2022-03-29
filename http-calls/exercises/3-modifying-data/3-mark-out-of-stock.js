92d9a89a115efef261c600b97a736a62553c8adf
Aviv <aviv.dror@gmail.com>
2022-03-29 12:08:16 +0200

    solutiond

--- http-calls/exercises/3-modifying-data/3-mark-out-of-stock.js
+++ http-calls/exercises/3-modifying-data/3-mark-out-of-stock.js
@@ -1,11 +1,25 @@
 /**
- * Complete the following function 
- * that can be used to mark a product 
+ * Complete the following function
+ * that can be used to mark a product
  * as out of stock
  * @param {number} productId the id of the product that is out of stuck
  */
 async function markOutOfStock(productId = 0) {
- _
+  const path = `products/${productId}`;
+  const body = {
+    data: {
+      outOfStock: true,
+    }, // question: how can we reach this field when it is inside of 'attributes'?
+  };
+  const url = `http://localhost:1337/api/${path}`;
+  const response = await fetch(url, {
+    method: 'PUT',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(body),
+  });
+  return response.json();
 }
 
 markOutOfStock(7);
