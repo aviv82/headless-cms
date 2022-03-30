b8930debbe420f35efb6c2d377db4ed6ab257322
Aviv <aviv.dror@gmail.com>
2022-03-30 11:58:10 +0200

    solutions

--- http-calls/exercises/3-modifying-data/7-remove-product.js
+++ http-calls/exercises/3-modifying-data/7-remove-product.js
@@ -3,7 +3,15 @@
  * removes the product with the given id
  */
 async function removeProduct(productId) {
-  _
+  const path = `products/${productId}`;
+  const url = `http://localhost:1337/api/${path}`;
+  const response = await fetch(url, {
+    method: 'DELETE',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  });
+  return response.json();
 }
 
-_
+removeProduct(14);
