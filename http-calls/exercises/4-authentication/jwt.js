18db4930fb6239d1b9759ed02057d79ac65ad5db
Aviv <aviv.dror@gmail.com>
2022-04-08 13:22:43 +0200

    token

--- http-calls/exercises/4-authentication/jwt.js
+++ http-calls/exercises/4-authentication/jwt.js
@@ -1,2 +1,2 @@
-export const jwt =
+export const token =
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6Mywia…zODR9.6w4aNjeca_qi4czHUitcH-HxYrig06CEHg63e9A4-wQ";
--- http-calls/exercises/4-authentication/product-authentication.js
+++ http-calls/exercises/4-authentication/product-authentication.js
@@ -1,1 +1,27 @@
-import { jwt } from "./jwt.js";
+async function authcreateNewproduct(name, description, price) {
+  const path = 'products';
+  const body = {
+    name: name,
+    description: description,
+    price: price,
+  };
+
+  const token =
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6Mywia…zODR9.6w4aNjeca_qi4czHUitcH-HxYrig06CEHg63e9A4-wQ'; // JWT
+
+  const url = `http://localhost:1337/api/${path}`;
+  const response = await fetch(url, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      authorization: `Bearer ${token}`,
+    },
+    body: JSON.stringify(body),
+  });
+  return response.json();
+}
+
+authcreateNewproduct(
+  'authorised product ',
+  'This is a cool product made by an api call after authentication',
+);
